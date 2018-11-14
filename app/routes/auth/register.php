2c156b95f58ff43cb98f51b46e52e2833663a025
mukhulaazam <mukhulaazam@gmail.com>
2018-11-15 00:17:47 +0600

    Register is almost done next is

--- app/routes/auth/register.php
+++ app/routes/auth/register.php
@@ -1,6 +1,26 @@
 <?php
 
     $app->get('/register', function() use($app) {
-        $app->rendar('auth/register.php');
+        $app->render('auth/register.php');
     })->name('register');
+
+    $app->post('/register', function() use($app) {
+        
+        $request = $app->request();
+
+        $email = $request->post('email');
+        $username = $request->post('username');
+        $password = $request->post('password');
+        $passwordConfirm = $request->post('password_confirm');
+
+        $app->user->create([
+            'email' => $email,
+            'username' => $usernaem,
+            'password' => $app->hash->password($password)
+        ]);
+
+        $app->flash('global', 'You have to Registered ');
+        $app->response->redirect($app->urlFor('home'));
+
+    })->name('register.post');
 
--- app/views/templates/partials/navigation.php
+++ app/views/templates/partials/navigation.php
@@ -1,3 +1,4 @@
 <ul>
     <li><a href="{{ urlFor('home') }}">home</a></li>
+    <li><a href="{{ urlFor('register') }}">Register</a></li>
 </ul>
